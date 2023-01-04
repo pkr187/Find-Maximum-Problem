bbe12aec42b94b7ed5fbc8480a081e14b88607fc
pkr187 <pankajroman187@gmail.com>
2023-01-04 10:03:54 +0530

    [pankaj] Add:FindMaxInt

---
+++ Generic/FindMaxNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public class FindMaxNumber
+    {
+        //Use maximum int number 
+        public int FindMaxIntNumber(int firstNum, int secondNum, int thirdNum)
+        {
+            if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0)
+                return firstNum;
+            if (secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) > 0)
+                return secondNum;
+            else
+                return thirdNum;
+        }
+    }
+}
---
+++ Generic/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Generic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Text Maximum Number Problems Statement");
+        }
+    }
+}
