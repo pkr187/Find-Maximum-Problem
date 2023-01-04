5b82ea8c63df721a8585a2151e900591abd64258
pkr187 <pankajroman187@gmail.com>
2023-01-04 10:11:16 +0530

    [pankaj] Add:FindMaxFloat

--- Generic/FindMaxFloat.cs
+++ Generic/FindMaxFloat.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace Generic
 {
-    public class FindMaxNumber
+    public class FindMaxFloat
     {
         //Use maximum int number 
         public int FindMaxIntNumber(int firstNum, int secondNum, int thirdNum)
+        {
+            if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0)
+                return firstNum;
+            if (secondNum.CompareTo(firstNum) > 0 && secondNum.CompareTo(thirdNum) > 0)
+                return secondNum;
+            else
+                return thirdNum;
+        }
+        //Use maximum float number
+        public float FindMaxFloatNumber(float firstNum, float secondNum, float thirdNum)
         {
             if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0)
                 return firstNum;
